8330c3afc19bba9996e7d82164a8045cd0192989
Frédéric Hardy <frederic.hardy@mageekbox.net>
2021-05-27 17:31:26 +0200

    Init done, have a good journey!

---
+++ tests/units/src/block/reference.php
@@ -0,0 +1,15 @@
+<?php namespace your\php\namepsace\here\tests\units\block;
+
+require __DIR__ . '/../../runner.php';
+
+use eastoriented\tests\units;
+
+class reference extends units\test
+{
+	function testClass()
+	{
+		$this->testedClass
+			->implemetns('eastoriented\php\block')
+		;
+	}
+}
